c286cfdac5a1ddf35dd53357158f2881ecba0456
Dimas Hafizh <dimashwp46@gmail.com>
2024-01-02 01:07:21 +0700

    ChatApp

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import LoginForm from './components/LoginForm';
+import ChatFeed from './components/ChatFeed';
+
+import './App.css';
+
+const App = () => {
+    if(!localStorage.getItem('username')) return <LoginForm />
+
+    return (
+        <ChatEngine
+            height="100vh"
+            projectID="543dab70-0a62-4804-8855-e77a1b06735d"
+            userName={localStorage.getItem('username')}
+            userSecret={localStorage.getItem('password')}
+            renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+        />
+    );
+}
+
+export default App;
